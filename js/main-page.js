d0f4ecc5903e3d21cba54e688729c141bb8d14c1
Omnomnomik <alexander.k.mailbox@gmail.com>
2021-05-22 00:34:51 +0300

    just fixing the main

--- js/main-page.js
+++ js/main-page.js
@@ -1,11 +1,20 @@
-function field_increment(increment) {
+function fieldIncrement(increment) {
     let value = Number(document.getElementById('count').value.replace(" чел.", ""));
 
     if (increment === true)
         value++;
     else
-        if (value > 1)
-            value--;
+        if (value > 1) value--;
 
     document.getElementById('count').value = value + ' чел.';
+}
+
+function swapDirection() {
+    let inputFrom = document.getElementById('fieldFrom');
+    let inputTo = document.getElementById('fieldTo');
+    let swap = '';
+
+    swap = inputFrom.value;
+    inputFrom.value = inputTo.value;
+    inputTo.value = swap;
 }
